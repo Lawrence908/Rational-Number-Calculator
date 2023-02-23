721aa4c963322ba54a208a8a16ac8bc900fbedbb
Rafe Saltman <577688708@viu.ca>
2023-02-23 02:08:05 +0000

    Created main.cpp and main.h

---
+++ main.cpp
@@ -0,0 +1,103 @@
+/** Rational Calculator
+ *
+ *  @file main.cpp
+ *  @brief A calculator for rational number arithmetic.
+ *
+ *  
+ *
+ *  @author Rafe Saltman, Chris Lawrence, and Santiago Daza
+ *  @version 0.1.0
+ *  @date Feb 22, 2023
+ */
+
+
+// Includes
+
+#include "main.hpp"
+#include <iostream>
+#include <cstdint>
+#include <regex>
+#include <string>
+using namespace std;
+
+
+
+int main () {
+  RationalExpression rex = generateExpression();
+  
+  rex = evaluate(rex);
+  
+  Ratio * result;
+  if (rex.knownRatio) {
+    result = rex.knownRatio;
+    cout << result->top << " / " << result->bottom << endl;
+  } else {
+    
+  }
+  
+  
+  return 0;
+}
+
+
+// Generates a rational expression based on user input.
+RationalExpression generateExpression () {
+  RationalExpression rex;
+  string input;
+  cout << "Enter an arithmetic expression:" << endl;
+  getline(cin, input);
+  
+  int length = 0;
+  while (input[length]) {
+    length++;
+  }
+  
+  string token[length];
+  tokenize(input, length, token);
+  
+  
+  rex = interpret(token);
+  return rex;
+}
+
+
+// Breaks a string into an array of tokens that are parts of an arithmetic expression.
+void tokenize (string input, int length, string token[]) {
+  regex expr_regex("\\d+|\\+|\\-|\\*|\\/|\\(|\\)");
+  
+  sregex_iterator it(input.begin(), input.end(), expr_regex);
+  sregex_iterator end;
+  
+  int i=0;
+  
+  while (it != end) {
+    token[i] = it->str();
+    cout << token[i] << " ";
+    it++;
+    i++;
+  }
+  cout << endl;
+}
+
+
+// Takes a string with arithmetic in S-expression form, interprets it as a rational expression, and returns that expression.
+RationalExpression interpret (string input, int first, int last) {
+  RationalExpression rex;
+  
+  for (int i = first; i <= last; i++) {
+  }
+  
+  return rex;
+}
+
+
+// Takes an expression without a known ratio and returns one without operands.
+RationalExpression evaluate (RationalExpression rex) {
+  return rex;
+}
+
+
+// Takes a fraction and returns its reduced fraction.
+Ratio reduce (Ratio fraction) {
+  return fraction;
+}
---
+++ main.h
@@ -0,0 +1,47 @@
+
+//
+//  main.hpp
+//  
+//
+//  Created by Rafe Saltman on 2023-02-22.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+
+// Data structures
+struct Ratio {
+  int64_t top;     // The numerator.
+  int64_t bottom;  // The denominator.
+};
+
+struct RationalExpression {
+  Ratio * knownRatio = 0;
+  char * operation = 0;
+  RationalExpression * leftOperand = 0;
+  RationalExpression * rightOperand = 0;
+};
+
+// Function declarations
+
+// Generates a rational expression based on user input.
+RationalExpression generateExpression ();
+
+// Breaks a string into an array of tokens that are parts of an arithmetic expression.
+void tokenize (string input, int length, string token[]);
+
+// Takes a string, interprets it as a rational expression, and returns that expression.
+RationalExpression interpret (string * token);
+
+// Takes an expression without a known ratio and returns one without operands.
+RationalExpression evaluate (RationalExpression rex);
+
+// Takes a fraction and returns its reduced fraction.
+Ratio reduce (Ratio fraction);
+
+#endif /* main_hpp */
